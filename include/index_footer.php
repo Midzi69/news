2a6eb72fdba5cb8e60696999ae72a110ffb9cd06
Miloš Tordai <76227243+Midzi69@users.noreply.github.com>
2023-07-11 14:17:00 +0200

    Add files via upload

---
+++ include/index_footer.php
@@ -0,0 +1,20 @@
+<div class="p-4">
+    <h4 class="fst-italic">Elsewhere</h4>
+    <ol class="list-unstyled">
+        <li><a href="#">GitHub</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Facebook</a></li>
+    </ol>
+</div>
+</div>
+</div>
+</div>
+
+</main>
+
+<footer class="py-5 text-center text-body-secondary bg-body-tertiary">
+
+</footer>
+
+</body>
+</html>
