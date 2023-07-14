48cdbb618698c07261983f669acb86fcd8a5ffac
Miloš Tordai <76227243+Midzi69@users.noreply.github.com>
2023-07-14 15:26:45 +0200

    Update header.php

--- include/header.php
+++ include/header.php
@@ -17,7 +17,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
     </script>
-    
+
+      <?php
+
+      require_once 'vendor/autoload.php';
+
+      ?>
 
 <link href="/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
