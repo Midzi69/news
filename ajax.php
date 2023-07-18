af1173f2fe13db416df6acbc804bb36c3581fb07
Miloš Tordai <76227243+Midzi69@users.noreply.github.com>
2023-07-18 19:25:25 +0200

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,11 +1,12 @@
 <?php
-require_once 'vendor/autoload.php';
+#require_once 'vendor/autoload.php';
+require_once 'db/conn.php';
 
 $country_id = $_POST['country_id'];
-$query= "select * from state where country_id='".$country_id."' ";
+$query= "select * from states where country_id='".$country_id."' ";
 $result= mysqli_query($conn,$query);
 
 while ($row= mysqli_fetch_array($result)) {
     ?>
-    <option value='<?php echo $row['id'] ?>'><?php echo $row['state']; ?></option>;
+    <option value='<?php echo $row['id'] ?>'><?php echo $row['name']; ?></option>;
 <?php } ?>
