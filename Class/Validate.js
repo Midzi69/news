ab134eed4d4101db0eb67fcf9cf0eae0f42fdbc9
Miloš Tordai <76227243+Midzi69@users.noreply.github.com>
2023-07-26 19:33:06 +0200

    Delete Class directory

--- Class/Validate.js
+++
@@ -1,9 +0,0 @@
-class Validate {
-     validate() {
-        let x = document.forms['categoryform']['category'].value;
-        if(x == ""){
-            alert(`category Must be filled out`);
-            return false;
-        }
-    }
-}
