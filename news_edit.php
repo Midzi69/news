4b3869897ed8d51dae728a21ffd66d029ff4a944
Miloš Tordai <76227243+Midzi69@users.noreply.github.com>
2023-07-26 19:37:32 +0200

    Update news_edit.php

--- news_edit.php
+++ news_edit.php
@@ -23,6 +23,7 @@ include('include/header.php');
         $date = $row['date'];
         $thumbnail = $row['thumbnail'];
         $category = $row['category'];
+
     }
 
 ?>
@@ -30,60 +31,16 @@ include('include/header.php');
 
 <div style=" width: 50%;  margin-left: 10%; margin-top: 10%;">
 
-    <div>
-        <ul class="breadcrumb">
-            <li class="breadcrumb-item active"><a href="home.php">Home</a></li>
-            <li class="breadcrumb-item active"><a href="news.php">News</a></li>
-            <li class="breadcrumb-item active">Add News</li>
-        </ul>
-    </div>
-
-    <form action="news_edit.php" method="post" name="categoryform" enctype="multipart/form-data" onsubmit=" return validate() ">
-        <h1>Update News</h1>
-
-        <div class="mb-3">
-            <label for="email" class="form-label">Title: </label>
-            <input type="text" name="title" class="form-control" id="email" value="<?php echo $title;?>" placeholder="Enter Title: ">
-        </div>
-        <div class="mb-3">
-            <label for="comment">Description: </label>
-            <textarea class="form-control" name="description"  rows="5" id="comment" placeholder="Enter Description: "><?php echo $description;?></textarea>
-        </div>
-
-        <div class="mb-3">
-            <label for="email" class="form-label">Date: </label>
-            <input type="date" name="date" value="<?php echo $date;?>" class="form-control" id="email" >
-        </div>
-
-        <div class="mb-3">
-            <label for="email" class="form-label">Thumbnail: </label>
-            <input type="file" value="<?php echo $thumbnail;?>" name="thumbnail" class="form-control img-thumbnail" id="email" >
-            <img class="img img-thumbnail" src="img/<?php echo $thumbnail;?>" alt="" width="100" height="100">
-        </div>
-
-        <input type="hidden" name="id" value="<?php echo $_GET['edit']; ?>">
-
-        <div class="mb-3">
-            <label for="email" class="form-label">Choose Category: </label>
 
 
+    <?php
 
-            <select class="form-control" name="category">
-                <?php
+    # EDIT VESTI
+    require_once 'Class/Vest.php';
+    $newsForm = new NewsPage($conn, $_GET['page']);
+    $newsForm->edit($title, $description, $date, $thumbnail, $category, $id, $conn);
 
-
-
-                $query=mysqli_query($conn, "select * from category");
-                while($row = mysqli_fetch_array($query)) {
-                    ?>
-                    <option value="<?php echo $row['category_name']; ?>"><?php echo $row['category_name']; ?></option>
-                <?php } ?>
-
-            </select>
-        </div>
-
-        <input type="submit" name="submit" class="btn btn-primary" value="Update">
-    </form>
+    ?>
 
     <script>
 
@@ -124,14 +81,13 @@ include('include/footer.php');
 
 
     if(isset($_POST['submit'])) {
-        $id = $_POST['id'];
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-        $date = $_POST['date'];
+        $id = mysqli_real_escape_string($conn, $_POST['id']);
+        $title = mysqli_real_escape_string($conn, $_POST['title']);
+        $description = mysqli_real_escape_string($conn, $_POST['description']);
+        $date = mysqli_real_escape_string($conn, $_POST['date']);
         $thumbnail = $_FILES['thumbnail']['name'];
         $tmp_thumbnail = $_FILES['thumbnail']['tmp_name'];
-        $category = $_POST['category'];
-        move_uploaded_file($tmp_thumbnail, "img/$thumbnail");
+        $category = mysqli_real_escape_string($conn, $_POST['category']);
 
         $sql = mysqli_query($conn, "update news set title='$title',description='$description',date='$date', thumbnail='$thumbnail', category='$category' where id='$id'");
 
