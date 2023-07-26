e0793582f5c213f1615870c86e18c159155e79e3
Miloš Tordai <76227243+Midzi69@users.noreply.github.com>
2023-07-26 19:36:33 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -28,20 +28,13 @@ include('include/header.php');
 
     <div style=" width: 50%;  margin-left: 10%; margin-top: 10%;">
 
-        <form action="edit.php" method="post" name="categoryform" onsubmit=" return validate() ">
-            <h1>Update Category</h1>
+        <?php
 
-            <div class="mb-3">
-                <label for="email" class="form-label">Category: </label>
-                <input type="text" name="category" class="form-control" id="email" value="<?php echo $category; ?>">
-            </div>
-            <div class="mb-3">
-                <label for="comment">Comment: </label>
-                <textarea class="form-control"  name="des" rows="5" id="comment"><?php echo $des;?></textarea>
-            </div>
-            <input type="hidden" name="id" value="<?php echo $_GET['edit']?>">
-            <input type="submit" name="submit" class="btn btn-primary" value="Update Category">
-        </form>
+        require_once 'Class/Category.php';
+        $newsForm = new Category($conn);
+        $newsForm->edit($category, $des);
+
+        ?>
 
         <script>
 
@@ -59,9 +52,9 @@ include('include/header.php');
 <?php
 
 if (isset($_POST['submit'])){
-    $id = $_POST['id'];
-    $category = $_POST['category'];
-    $des=$_POST['des'];
+    $id = mysqli_real_escape_string($conn, $_POST['id']);
+    $category = mysqli_real_escape_string($conn, $_POST['category']);
+    $des = mysqli_real_escape_string($conn, $_POST['des']);
 
     $query1 = mysqli_query($conn, "update category set category_name='$category', des='$des' where id='$id' ");
 
