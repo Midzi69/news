022a89ed39c0be9a31e1dd6d21060e9f853c63ea
Miloš Tordai <76227243+Midzi69@users.noreply.github.com>
2023-07-26 19:38:59 +0200

    Update single-page.php

--- single-page.php
+++ single-page.php
@@ -125,47 +125,13 @@
                     </blockquote>
                     <hr>
 
-                    <form action="single-page.php" method="post">
+                    <?php
 
-                        <input type="text" name="name" class="form-control my-4 py-2" id="name" placeholder="Name: ">
-                        <textarea class="form-control" name="comment"  rows="5" id="comment" placeholder="Enter Comment: "></textarea>
+                    require_once 'Class/Comment.php';
+                    $newsForm = new Comment($conn);
+                    $newsForm->add($conn);
 
-                        <div class="mt-3">
-                            <input type="submit" name="submit" class="btn btn-primary" value="Post Comment">
-                        </div>
-
-                    </form>
-
-                    <hr>
-
-                    <h1>Comments</h1>
-
-                    <table class="table table-bordered">
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Comment</th>
-                        </tr>
-                        <?php
-
-
-
-                        $query = mysqli_query($conn, "select * from comment");
-                        while($row = mysqli_fetch_array($query)){
-
-                            ?>
-                            <tr>
-                                <td><?php echo $row['id'];?></td>
-                                <td><?php echo $row['name'];?></td>
-                                <td><?php echo $row['comment'];?></td>
-                            </tr>
-
-
-
-
-
-                        <?php } ?>
-                    </table>
+                    ?>
 
                 </article>
 
@@ -240,7 +206,6 @@
 
                     if($query1) {
                         echo "<script>alert(`Comment Posted!`);</script>";
-                        echo "<script >window.location = 'single-page.php';</script>";
                     } else {
                         echo "<script>alert(`Try again!`);</script>";
                     }
