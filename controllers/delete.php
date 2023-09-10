fc272f5a7dce0d8da3e1d6fa9646693ca8ed67d9
Miloš Tordai <milostordai@gmail.com>
2023-09-10 22:11:29 +0200

    Delete

--- controllers/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'vendor/autoload.php';
-    require_once 'Class/Category.php';
-    $newsForm = new Category($conn);
-    $newsForm->delete($conn);
-
-
-
-?>
--- controllers/vest.php
+++
@@ -1,7 +0,0 @@
-<?php 
-error_reporting(0);
-require 'Class/Vest.php';
-$newsPage = new NewsPage($conn, $_GET['page']);
-$newsPage->render();
-
-?>
